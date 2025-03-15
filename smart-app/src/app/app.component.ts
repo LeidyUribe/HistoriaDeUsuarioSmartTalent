4eef6cd33c0284380584cf48c6dd3fd68f8374eb
LeidyUribe <leidyu.f@gmail.com>
2025-03-15 16:23:53 -0500

    Limpieza de codigo y algunos estilos

--- smart-app/src/app/app.component.ts
+++ smart-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { TaskListComponent } from './task-list/task-list.component';
 import { TaskFormComponent } from './task-form/task-form.component';
@@ -8,22 +9,21 @@ import { TaskFilterComponent } from './task-filter/task-filter.component';
 
 @Component({
   selector: 'app-root',
-  imports: [TaskFormComponent, TaskListComponent,TaskFilterComponent],
+  imports: [
+    CommonModule,
+    TaskFormComponent,
+    TaskListComponent,
+    TaskFilterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-
 export class AppComponent {
-  alertMessage: string | null = null;
-  alertType: any = null;
   currentFilter: string = 'all';
-
-
-  constructor(
-    private taskService: TaskService) {}
+  constructor(private taskService: TaskService) {}
 
   title = 'Gestión de tareas';
-  
+
   onFilterChange(filter: string): void {
     this.currentFilter = filter;
   }
